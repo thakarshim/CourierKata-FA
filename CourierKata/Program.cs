f92dfe94250be689f6c0149aed693857e603fda6
Thakarshi makwana <thakarshi.makwana@autorama.co.uk>
2019-03-30 20:27:35 +0000

    code to take user input has been added

--- CourierKata/Program.cs
+++ CourierKata/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CourierKata
 {
@@ -10,7 +9,37 @@ namespace CourierKata
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Courier Kata");
+            Console.WriteLine("Add Parcel Details");
+            Courier courier = new Courier();
+            AddParcelDetail(courier);
+            Console.ReadKey();
+        }
 
+        private static void AddParcelDetail(Courier courier)
+        {
+            string AddMoreParcel = "";
+            do
+            {
+                CourierKata model = new CourierKata();
+                #region Input Parcel Detail
+                Console.Write("Enter Height : ");
+
+                decimal.TryParse(Console.ReadLine(), NumberStyles.AllowDecimalPoint, CultureInfo.DefaultThreadCurrentCulture, out decimal height);
+                model.Height = height;
+                Console.Write("Enter Width : ");
+                decimal.TryParse(Console.ReadLine(), NumberStyles.AllowDecimalPoint, CultureInfo.DefaultThreadCurrentCulture, out decimal width);
+                model.Width = width;
+
+                Console.Write("Enter Length : ");
+                decimal.TryParse(Console.ReadLine(), NumberStyles.AllowDecimalPoint, CultureInfo.DefaultThreadCurrentCulture, out decimal length);
+                model.Length = length;
+
+                Console.Write("Enter Parcel Weight(kg) : ");
+                decimal.TryParse(Console.ReadLine(), NumberStyles.AllowDecimalPoint, CultureInfo.DefaultThreadCurrentCulture, out decimal weight);
+
+                #endregion
+            }
         }
     }
 }
